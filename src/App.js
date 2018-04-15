2287ce0770b3741c169d8857d02070a42623539b
oim5nu <oim5nu@gmail.com>
2018-04-15 14:34:47 +1000

    move clarifai call to server side

--- src/App.js
+++ src/App.js
@@ -7,12 +7,24 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import './App.css';
 
-const app = new Clarifai.App({
-  apiKey: 'a467ff10fb6c409a8db43ca587fb9041'
-});
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''        
+  }
+};
+
+
 
 const particlesOptions = {
   particles: {
@@ -44,20 +56,7 @@ const particlesOptions = {
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialState;
   }
 
   // componentDidMount() {
@@ -66,16 +65,30 @@ class App extends Component {
   //     .then(console.log());
   // }
 
+  // loadUser = (data) => {
+  //   this.setState({
+  //     user: {
+  //       id: data.id,
+  //       name: data.name,
+  //       email: data.email,
+  //       entries: data.entries,
+  //       joined: data.joined
+  //     } 
+  //   });
+  // }
+
   loadUser = (data) => {
-    this.setState({
-      user: {
+    let newState = Object.assign({}, 
+      this.state,
+      { user: {
         id: data.id,
         name: data.name,
         email: data.email,
         entries: data.entries,
-        joined: data.joined
-      } 
-    });
+        joined: data.joined        
+      }}
+    );
+    this.setState(newState);
   }
 
   calculateFaceLocation = (data) => {
@@ -83,7 +96,7 @@ class App extends Component {
     const image = document.getElementById('inputImage');
     const width = Number(image.width);
     const height = Number(image.height);
-    console.log(width, height);
+    // console.log(width, height);
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
@@ -93,19 +106,27 @@ class App extends Component {
   }
 
   displayFaceBox = (box) => {
-    this.setState({box: box});
+    let newState = Object.assign({}, 
+      this.state,
+      { box: {...box} }
+    );
+    this.setState(newState);
   }
 
   onInputChange = (event) => {
     this.setState({input: event.target.value});
   }
 
-  onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input})
-    app.models.predict(
-      Clarifai.FACE_DETECT_MODEL, 
-      this.state.input
-    ).then(response => {
+  imageUrlApi = () => {
+    fetch('http://localhost:3000/imageurl', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+    .then(response => response.json())
+    .then(response => {
       //console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
       if (response) {
         fetch('http://localhost:3000/image', {
@@ -117,19 +138,33 @@ class App extends Component {
         })
         .then(response => response.json())
         .then(count => {
-          this.setState(Object.assign(this.state.user, { entries: count } ))
+          let newState = Object.assign({}, this.state, 
+            { user: { 
+              ...this.state.user, 
+              entries: count 
+            }}
+          );
+          this.setState(newState);
+          //this.setState(Object.assign(this.state.user, { entries: count } ))
         })
+        .catch(error => console.log(error))
       }
-      const box = this.calculateFaceLocation(response);
-      this.displayFaceBox(box);
+      // const box = this.calculateFaceLocation(response);
+      this.displayFaceBox(this.calculateFaceLocation(response));
     }).catch(err => {
       console.log(err);
-    });    
+    });
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input}, () => {
+      this.imageUrlApi();
+    }); 
   }
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false});
+      this.setState(initialState);
     } else if (route === 'home') {
       this.setState({isSignedIn: true});
     }
@@ -139,6 +174,7 @@ class App extends Component {
 
   render() {
     const { isSignedIn, imageUrl, route, box, user } = this.state;
+    //console.log('this.state', this.state);
     return (
       <div className="App">
         <Particles className="particles"
@@ -155,7 +191,7 @@ class App extends Component {
             />
             <FaceRecognition box={box} imageUrl={imageUrl} />
           </div>
-          : ( route === 'signin' ?
+          : ( route === 'signin' || route === 'signout' ?
           <Signin onRouteChange={this.onRouteChange} loadUser={this.loadUser} /> : 
           <Register onRouteChange={this.onRouteChange} loadUser={this.loadUser}/>
           )
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
   return (
     <div>
       <p className="f3">
-        {'This Magic Robot will detect faces in your pictures. Git it a try.'}
+        {'This Magic Robot will detect faces in your pictures. Give it a try.'}
       </p>
       <div className="center">
         <div className="form polka center pa4 br3 shadow-5">
